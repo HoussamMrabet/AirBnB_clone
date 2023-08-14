69ed62e10a56e63cf00e3d92859d8c0fef7f63a8
HoussamMrabet <dodslim100@gmail.com>
2023-08-14 03:41:44 +0100

    basic console program

---
+++ console.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb)'
+
+    def do_quit(self, command):
+        exit()
+
+    def help_quit(self):
+        print("Exits the program with formatting")
+
+    def do_EOF(self, arg):
+        exit()
+
+    def help_EOF(self):
+        print("Exits the program without formatting")
+
+    def emptyline(self):
+        pass
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
