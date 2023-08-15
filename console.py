a5ba284f80009588918121bc68a6da192cdfa2be
HoussamMrabet <dodslim100@gmail.com>
2023-08-15 20:02:59 +0100

    basic console program

--- console.py
+++ console.py
@@ -19,6 +19,12 @@ class HBNBCommand(cmd.Cmd):
         print()  # Print a newline before exiting
         return True
 
+    def emptyline(self):
+        """
+        Do nothing on an empty line
+        """
+        pass
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
