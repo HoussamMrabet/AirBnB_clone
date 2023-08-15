f962d4dcaf5a6b86c06c88936f0266a07dc4d652
HoussamMrabet <dodslim100@gmail.com>
2023-08-15 19:59:00 +0100

    basic console program

--- console.py
+++ console.py
@@ -1,18 +1,23 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    prompt = "(hbnb)"
+    prompt = "(hbnb) "
 
-    def do_quit(self, arg):
+    def do_quit(self, args):
+        """
+        Quit command to exit the program
+        """
         return True
 
-    def do_EOF(self, arg):
+    def do_EOF(self, args):
+        """
+        Exit the program on EOF (Ctrl+D)
+        """
+        print()  # Print a newline before exiting
         return True
-
-    def emptyline(self):
-        pass
 
 
 if __name__ == "__main__":
