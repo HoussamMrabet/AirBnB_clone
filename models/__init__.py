97cf46a0a34acc1ae6ab4a41ffd29f7a6a9dd7ab
HoussamMrabet <dodslim100@gmail.com>
2023-08-17 18:52:44 +0100

    7. Console 0.1

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
 
 
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
 import json
 
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
-
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Returns a dictionary of models currently in storage"""
         return FileStorage.__objects
 
     def new(self, obj):
-        """Adds new object to storage dictionary"""
         self.all().update({obj.to_dict()["__class__"] + "." + obj.id: obj})
 
     def save(self):
-        """Saves storage dictionary to file"""
         with open(FileStorage.__file_path, "w") as f:
             temp = {}
             temp.update(FileStorage.__objects)
@@ -27,7 +21,6 @@ class FileStorage:
             json.dump(temp, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
         from models.base_model import BaseModel
         from models.user import User
         from models.place import Place
